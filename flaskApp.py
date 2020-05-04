75c8a45172b0661c4129752c8d3fdcfba0d908c3
Hamza Mustafa Alvi <i181421@nu.edu.pk>
2020-05-05 01:54:07 +0500

    Modified flaskapp to show name and improve output.

--- flaskApp.py
+++ flaskApp.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hostname = " + socket.gethostname() + ",  Roll No = i18-1421"
+    return ("<html><head><style>body {text-align: center;}</style></head><body>"
+        "<b>Roll No = </b>i18-1421 <br /> <br /> " + 
+        "<b>Name = </b> Hamza Mustafa Alvi <br /> <br />" +
+        "<b>Hostname = </b>" + socket.gethostname() +
+        "</body></html>"
+        )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
