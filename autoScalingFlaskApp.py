bf9319f299a0ff6d4e7b713cea05f1c488f81aae
Hamza Mustafa Alvi <i181421@nu.edu.pk>
2020-05-05 01:25:00 +0500

    The script to do the tasks listed in issue #29 has been written and it has been tested to be working with the changes made in haproxy config file.

--- autoScalingFlaskApp.py
+++ autoScalingFlaskApp.py
@@ -2,18 +2,12 @@ import psutil
 from subprocess import Popen, PIPE
 import time
 
-def addServer(port, serverN):
+def addServer(port):
     output = Popen('docker run -d -p '+port+':5000 project-phase-2:latest', shell=True, stdout=PIPE)
     dockerId = output.communicate()[0].decode('utf-8')
-    output = Popen('set server flaskAppBackend/docker'+str(serverN)+' addr 127.0.0.1 port '+port, shell=True, stdout=PIPE)
-    output = output.communicate()[0].decode('utf-8')
-    output = Popen('set server flaskAppBackend/docker'+str(serverN)+' state ready', shell=True, stdout=PIPE)
-    output = output.communicate()[0].decode('utf-8')
     return dockerId
 
-def removeServer(port, serverN, dockerId):
-    output = Popen('set server flaskAppBackend/docker'+str(serverN)+' state maint', shell=True, stdout=PIPE)
-    output = output.communicate()[0].decode('utf-8')
+def removeServer(dockerId):
     output = Popen('docker stop '+dockerId, shell=True, stdout=PIPE)
     output = output.communicate()[0].decode('utf-8')
     output = Popen('docker rm '+dockerId, shell=True, stdout=PIPE)
@@ -32,7 +26,7 @@ dockerIds = []
 
 while(True):
     # Finding CPU load
-    cpuUtil = psutil.cpu_percent(interval=5)
+    cpuUtil = psutil.cpu_percent(interval=1)
 
     # Finding the number of containers that can exist
     N = int(cpuUtil // 10)
@@ -41,16 +35,16 @@ while(True):
 
     # Adding Containers if current number of containers are less than the required number
     for i in range( N - Nc ):
-        dId = addServer(dockerPortsList[Nc + i], Nc + i + 1)
+        dId = addServer(dockerPortsList[Nc + i])
         dockerIds += [dId]
         dockerPortsInUse += [dockerPortsList[Nc + i]]
 
     # Removing Containers if current number of containers are greater than the required number
     for i in range( Nc - N ):
-        removeServer(dockerPortsInUse[Nc - i - 1], Nc - i, dockerIds[Nc - i - 1])
+        removeServer(dockerIds[Nc - i - 1])
         del dockerIds[Nc - i - 1]
         del dockerPortsInUse[Nc - i - 1]
-    time.sleep(10)
+    time.sleep(5)
 
     print('In this iteration ',N-Nc if N-Nc > 0 else 0,'containers were created and ',Nc-N if Nc-N > 0 else 0,'containers were removed')
     print()
