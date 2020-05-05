73f206b4745fda50c1b85a743e4855f63eb6a22e
Hamza Mustafa Alvi <i181421@nu.edu.pk>
2020-05-05 13:22:42 +0500

    The requirements were not correctly met before. Edited the code to met the requirements correctly.

--- autoScalingFlaskApp.py
+++ autoScalingFlaskApp.py
@@ -2,26 +2,31 @@ import psutil
 from subprocess import Popen, PIPE
 import time
 
-def addServer(port):
-    output = Popen('docker run -d -p '+port+':5000 project-phase-2:latest', shell=True, stdout=PIPE)
+HAproxyCommandPart1 = 'echo "set server flaskAppBackend/docker'
+HAproxyCommandPart2 = '" | socat stdio tcp4-connect:127.0.0.1:9999'
+
+def addServer(serverN):
+    output = Popen('docker run -d project-phase-2:latest', shell=True, stdout=PIPE)
     dockerId = output.communicate()[0].decode('utf-8')
+    output = Popen('docker inspect -f "{{ .NetworkSettings.IPAddress }}" '+dockerId, shell=True, stdout=PIPE)
+    dockerIP = output.communicate()[0].decode('utf-8')
+    output = Popen(HAproxyCommandPart1 + serverN + ' addr ' + dockerIP + HAproxyCommandPart2, shell=True, stdout=PIPE)
+    output = output.communicate()[0].decode('utf-8')
+    output = Popen(HAproxyCommandPart1 + serverN + ' state ready' + HAproxyCommandPart2, shell=True, stdout=PIPE)
+    output = output.communicate()[0].decode('utf-8')
     return dockerId
 
-def removeServer(dockerId):
+def removeServer(dockerId, serverN):
+    output = Popen(HAproxyCommandPart1 + serverN + ' state maint' + HAproxyCommandPart2, shell=True, stdout=PIPE)
+    output = output.communicate()[0].decode('utf-8')
     output = Popen('docker stop '+dockerId, shell=True, stdout=PIPE)
     output = output.communicate()[0].decode('utf-8')
     output = Popen('docker rm '+dockerId, shell=True, stdout=PIPE)
     output = output.communicate()[0].decode('utf-8')
     pass
 
-# Generating maximum possible list of ports that we can use
-dockerPortsList = []
-for i in range(1,10):
-    dockerPortsList += ['500'+str(i)]
-dockerPortsList += ['5010']
-
-# Creating a list than will maintain the ports and Ids of containers that are currently running
-dockerPortsInUse = []
+# Creating a list than will maintain the docker numbers and Ids of containers that are currently running
+dockersInUse = []
 dockerIds = []
 
 while(True):
@@ -31,19 +36,19 @@ while(True):
     # Finding the number of containers that can exist
     N = int(cpuUtil // 10)
     N = 1 if N < 2 else N
-    Nc = len(dockerPortsInUse)
+    Nc = len(dockersInUse)
 
     # Adding Containers if current number of containers are less than the required number
     for i in range( N - Nc ):
-        dId = addServer(dockerPortsList[Nc + i])
+        dId = addServer(str(Nc + i + 1))
         dockerIds += [dId]
-        dockerPortsInUse += [dockerPortsList[Nc + i]]
+        dockersInUse += [Nc + i + 1]
 
     # Removing Containers if current number of containers are greater than the required number
     for i in range( Nc - N ):
-        removeServer(dockerIds[Nc - i - 1])
+        removeServer(dockerIds[Nc - i - 1], str(Nc - i))
         del dockerIds[Nc - i - 1]
-        del dockerPortsInUse[Nc - i - 1]
+        del dockersInUse[Nc - i - 1]
     time.sleep(5)
 
     print('In this iteration ',N-Nc if N-Nc > 0 else 0,'containers were created and ',Nc-N if Nc-N > 0 else 0,'containers were removed')
